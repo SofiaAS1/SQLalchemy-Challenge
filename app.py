24523ded29eb50949510c184a267864532b825e6
SAS 826 <sasotillo826@gmail.com>
2020-08-16 20:19:22 -0400

    completed home route on app.py

--- app.py
+++ app.py
@@ -3,32 +3,37 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    print("Server received request for 'Home' page...")
-    return "Welcome to my 'Home' page!"
+    return (
+        f"Welcome to The Climate App API!<br/>"
+        f"<br/>"
+        f"Available Routes are:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+        f"<br/>"
+        f"May Your Days Be Bright & Sunny!"
+    )
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    print("Server received request for 'precipitation' page...")
     return "Welcome to my 'Precipitation' page!"
 
 @app.route("/api/v1.0/stations")
 def stations():
-    print("Server received request for 'stations' page...")
     return "Welcome to my 'stations' page!"
 
 @app.route("/api/v1.0/tobs")
 def TObs():
-    print("Server received request for 'TObs' page...")
     return "Welcome to my 'TObs' page!"
 
 @app.route("/api/v1.0/<start>")
 def Start():
-    print("Server received request for 'Start' page...")
     return "Welcome to my 'Start' page!"
 
 @app.route("/api/v1.0/<start>/<end>")
 def SnE():
-    print("Server received request for 'SnE' page...")
     return "Welcome to my 'Start & End' page!"
 
 if __name__ == "__main__":
